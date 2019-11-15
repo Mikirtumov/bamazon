d32d9c79c4d857cab81d580b6bb288b9e019ade8
Sergo Mikirtumov <mikirtumov@gmail.com>
2019-11-15 16:44:28 -0500

    bamazon readme

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # bamazon
+
+**Creator**: `Sergo Mikirtumov`
+
+**Created on**: `Nov 15th 2019`
+
+## ABOUT THE APP
+`Bamazon` is a command line node app that takes in orders from customers and depletes stock from the store's inventory.
+
+## HOW TO USE LIRI
+
+![](demo.gif)
+
+**Screen-shot**
+![Results](/screenshot/Screenshot_1.PNG)
+
+![Results](/screenshot/Screenshot_2.PNG)
+
+![Results](/screenshot/Screenshot_3.PNG)
+
+## TECHNOLOGIES USED
+* Javascript
+* Nodejs
+* MySQL
+* Node packages:
+    * MySQL
+    * Inquirer
+    * Cli-table
+    * Chalk
+* Git
+* GitHub
--- bamazoncostumer.js
+++ bamazoncostumer.js
@@ -1,92 +1,129 @@
 var mysql = require('mysql');
 var Table = require('cli-table');
 var inquirer = require('inquirer');
+const chalk = require('chalk');
+ 
 
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
-  password : 'TiNuviEl3',
+  password : '',
   database : 'bamazon'
 });
  
-connection.connect();
- 
+connection.connect(function(error) {
+  if (error) throw error;
+  welcome();
+});
 
+
+function welcome(){
+  console.log(chalk.blue.bold("\n\t<<<<<<< WELCOME TO BAMAZON >>>>>>>"));
+  products()
+}
  
-var productsArr = [];
 
 function products(){
-    
-
-
-connection.query('SELECT * from products', function (error, results, fields) {
+connection.query('SELECT * from products', function (error, results){
     if (error) throw error;
     
     var table = new Table({
-        head: ['id', 'product','price','quantity'],
+        head: ['ID', 'Product','Price','Quantity'],
+        chars: { 'top': '═' , 'top-mid': '╤' , 'top-left': '╔' , 'top-right': '╗'
+        , 'bottom': '═' , 'bottom-mid': '╧' , 'bottom-left': '╚' , 'bottom-right': '╝'
+        , 'left': '║' , 'left-mid': '╟' , 'mid': '─' , 'mid-mid': '┼'
+        , 'right': '║' , 'right-mid': '╢' , 'middle': '│' },
+      
         style: {
-            head: ['yellow'],
+            head: ['green'],
             compact: false,
             colAligns: ['center'],
+
         }
     });
-    
+    console.log(chalk.bold("\n\tlist of items you can purchase"));
+    console.log("\t------------------------------\n");
     for (var i=0; i< results.length; i++){
-              table.push([results[i].item_id, results[i].product_name, results[i].price, results[i].stock_quantity]);
-        productsArr.push([results[i].item_id, results[i].product_name, results[i].price, results[i].stock_quantity]);
+              table.push([results[i].item_id, results[i].product_name, "$" + results[i].price, results[i].stock_quantity]);
+
     }
     console.log(table.toString());
-  });
-  var questions = [
+    appleProducts();
+  })
+}
+  
+  function appleProducts(){
+    inquirer.prompt([
     {
       type: 'input',
-      name: 'ID',
-      message: "What is product id number?"
+      name: "id",
+      message: "What is product id number?",
+      validate: function(value){
+        if (value!=="" && isNaN(value) == false &&  value<11 ) {
+            return true;
+        } else {
+            return chalk.bgRed("Enter a valid ID");
+        }
+    }
       
     },
     {
         type: 'input',
         name: 'quantity',
-        message: "How many units?"
-        
-    },
-];
-
-inquirer.prompt(questions)
-.then(answers => {
-  console.log(JSON.stringify(answers, null, '  '));
-  var costumerItem = [];
-  for (var i = 0; i < productsArr.length; i++){
-      
-      var costumerId = parseInt(answers.ID);
-      
-      if(costumerId == productsArr[i][0]){
-        costumerItem = productsArr[i];
-        console.log(costumerItem);
-        
+        message: "How many units?",
+        validate: function(value){
+          if (value!=="" && isNaN(value) == false) {
+              return true;
+          } else {
+              return chalk.bgRed("Enter a number");
+          }
       }
-      
-      
-
-  }
-    
-var selectedStock = parseInt(costumerItem[3]);
-var userStock = parseInt(answers.quantity);
-
-  checkStock(selectedStock,userStock);
-});
+ },
+])
+.then(function(answer){
+  var query = "SELECT item_id,product_name, stock_quantity, price FROM products WHERE item_id = ?";
+  connection.query(query, [answer.id], function(error, result){
+      for (var i = 0; i <result.length; i++) {
+          console.log(chalk.greenBright("Your request: ") + (result[i].product_name) + " " + ("quantity") + " " + (answer.quantity));
+          console.log("____________________________________")
+          if(result[i].stock_quantity <answer.quantity){
+              console.log (chalk.red("Not enough quantity"));
+              nextOption();
+          }
+          else {
+              
+              console.log(("Purchase made"));
+              var purchaseTotal = result[i].price * answer.quantity;
+              console.log(("Total COST is $" + purchaseTotal));
+            
+              var newQty = result[i].stock_quantity - answer.quantity;
+              console.log (chalk.green("Quantity remaining: ") + ( + " " + newQty));
+              console.log("____________________________________")
+              connection.query("UPDATE products SET stock_quantity = " + newQty +" WHERE item_id = " + result[i].item_id, function(error, result){
+              nextOption()
+              });
+          }
+      }
+  })
+})
 }
-products();
 
-
-
-function checkStock(databaseStock, costumerRequest){
-    
-    if (databaseStock>costumerRequest){
-        console.log("purchase made");
-    }
-    else{
-        console.log("Insufficient quantity!");
-    }
-
+function nextOption(){
+  inquirer.prompt([
+      {
+          name: "continue",
+          type: "confirm",
+          message:("Try again?")
+      }
+  ])
+  .then(function(response){
+      if (response.continue == true){
+          products();
+      }
+      else{
+          console.log ("Good Buy!");
+          connection.end();
+      }
+  })
 }
+
